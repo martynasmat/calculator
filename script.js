996963af96d47c7feeaaf0b195a64972c7ad8d2d
Martynas M <martynasmat03@gmail.com>
2022-06-28 17:35:48 +0300

    Finish functionality

--- script.js
+++ script.js
@@ -1,55 +1,105 @@
-let currentValueText = "";
 const currentValueElement = document.querySelector(".current-value");
 const previousValueElement = document.querySelector(".previous-value");
+const mathFunctions = {
+    "+": addFunction,
+    "-": subtractFunction,
+    "*": multiplyFunction,
+    "/": divideFunction,
+};
 
-const zeroButton = document.querySelector(".zero");
-zeroButton.addEventListener("click", function() {
-    currentValueElement.textContent += "0"; 
-});
-const oneButton = document.querySelector(".one");
-oneButton.addEventListener("click", function() {
-    currentValueElement.textContent += "1";
-});
-const twoButton = document.querySelector(".two");
-twoButton.addEventListener("click", function() {
-    currentValueElement.textContent += "2";
-});
-const threeButton = document.querySelector(".three");
-threeButton.addEventListener("click", function() {
-    currentValueElement.textContent += "3";
-});
-const fourButton = document.querySelector(".four");
-fourButton.addEventListener("click", function() {
-    currentValueElement.textContent += "4";
-});
-const fiveButton = document.querySelector(".five");
-fiveButton.addEventListener("click", function() {
-    currentValueElement.textContent += "5";
-});
-const sixButton = document.querySelector(".six");
-sixButton.addEventListener("click", function() {
-    currentValueElement.textContent += "6";
-});
-const sevenButton = document.querySelector(".seven");
-sevenButton.addEventListener("click", function() {
-    currentValueElement.textContent += "7";
-});
-const eightButton = document.querySelector(".eight");
-eightButton.addEventListener("click", function() {
-    currentValueElement.textContent += "8";
-});
-const nineButton = document.querySelector(".nine");
-nineButton.addEventListener("click", function() {
-    currentValueElement.textContent += "9";
-});
+function addFunction(num1, num2) {
+    if(num1 === "") {
+        num1 = 0;
+    };
+    if(num2 === "") {
+        num1 = 0;
+    };
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return (num1 + num2);
+};
+
+function subtractFunction(num1, num2) {
+    if(num1 === "") {
+        num1 = 0;
+    };
+    if(num2 === "") {
+        num1 = 0;
+    };
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return (num1 - num2);
+};
+
+function multiplyFunction(num1, num2) {
+    if(num1 === "") {
+        num1 = 1;
+    };
+    if(num2 === "") {
+        num1 = 1;
+    };
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return (num1 * num2);
+};
+
+function divideFunction(num1, num2) {
+    if(num1 === "") {
+        num1 = 1;
+    };
+    if(num2 === "") {
+        num1 = 1;
+    };
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return (num1 / num2);
+};
+
+function numberButtonClick(arg) {
+    currentValueElement.textContent += arg;
+};
+
+function functionButtonClick(arg) {
+    if(previousValueElement.textContent === "") {
+        previousValueElement.textContent = `${currentValueElement.textContent} ${arg}`;
+        currentValueElement.textContent = "";
+    }else {
+        previousValueElement.textContent = `${previousValueElement.textContent.split(' ')[0]} ${arg}`;
+    };
+};
+
+function equalsButtonClick() {
+    num1 = previousValueElement.textContent.split(' ')[0];
+    func = previousValueElement.textContent.split(' ')[1];
+    num2 = currentValueElement.textContent;
+    previousValueElement.textContent = "";
+    currentValueElement.textContent = mathFunctions[func](num1, num2);
+};
+
+const numberButtonArray = Array.from(document.querySelectorAll(".number-button"));
+numberButtonArray.forEach(button => button.addEventListener("click", function(evt) {
+    evt.arg = evt.target.value;
+        numberButtonClick(evt.arg);
+    })
+);
+
+const functionButtonArray = Array.from(document.querySelectorAll(".function-button"));
+functionButtonArray.forEach(button => button.addEventListener("click", function(evt) {
+        evt.arg = evt.target.value;
+        functionButtonClick(evt.arg);
+    })
+);
+
 const clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", function() {
     currentValueElement.textContent = "";
-    currentValueText = "";
     previousValueElement.textContent = "";
 });
+
 const deleteButton = document.querySelector(".delete");
 deleteButton.addEventListener("click", function() {
     currentValueElement.textContent = currentValueElement.textContent.slice(0, -1);
-    currentValueText = currentValueText.slice(0, -1);
 });
+
+const equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener("click", equalsButtonClick);
